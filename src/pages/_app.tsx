0ec41ddb1a8e9867358c35b7adb4713643951502
lucasbars <darklucassilva@gmail.com>
2021-07-15 20:45:57 -0300

    function play and pause complete

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,15 +10,21 @@ import { useState } from 'react';
 function MyApp({ Component, pageProps }) {
   const [episodeList, setEpisodeList] = useState([]);
   const [currentEpisodeIndex, setcurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setisPlaying] = useState(false);
 
   function play(episode) {
-    setEpisodeList([episode]),
+    setEpisodeList([episode]);
     setcurrentEpisodeIndex(0);
+    setisPlaying(true);
+  }
+
+  function togglePlay() {
+    setisPlaying(!isPlaying);
   }
 
 
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play , isPlaying, togglePlay}}>
       <div className={styles.wrapper}>
         <main>
           <Header />
